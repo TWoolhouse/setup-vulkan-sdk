2e584f0e2de337f129a11201c5d812a782173022
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-09-17 01:40:46 +0100

    invalid env vars

--- resolve.sh
+++ resolve.sh
@@ -6,6 +6,5 @@ echo "::notice title=Webservice::$url" >&2
 version=$(curl -sL $url)
 test -n "$version" || { echo "::error title=Invalid Response::[$?] $version" ; exit 1 ; }
 echo "::notice title=Version::$version"
+echo "::set-output name=version::$version"
 echo "::endgroup::"
-
-echo "vk_ver=$version" >> GITHUB_ENV
