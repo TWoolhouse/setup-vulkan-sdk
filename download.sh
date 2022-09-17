e59ea9fc91cb3550c2f929a4c0af143433f70a4f
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-09-17 02:07:47 +0100

    V1

--- download.sh
+++ download.sh
@@ -3,6 +3,6 @@
 echo "::group::Download"
 fname=vulkan-sdk$sdk_os_ext
 url=https://vulkan.lunarg.com/sdk/download/$vk_ver/$vk_os/$fname
-echo "::notice title=Starting Download::$url"
+echo "::notice title=Download::$url"
 curl -L -o $download_name $url
 echo "::endgroup::"
